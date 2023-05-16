522c92f3137e2f756e9b857a139c9ef3141458c5
luisguilheerme <lg.maroni@gmail.com>
2023-05-16 20:21:45 -0300

    Project created

---
+++ src/main/java/com/luisguilherme/ultrahostel/UltraHostelApplication.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.ultrahostel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltraHostelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltraHostelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisguilherme/ultrahostel/UltraHostelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.ultrahostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltraHostelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
